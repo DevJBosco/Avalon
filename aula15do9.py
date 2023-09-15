6c8a3ef215425af3db10e54ba465e68e70fb62e2
TURMA-A <contatojoaoboscop@gmail.com>
2023-09-15 11:24:15 -0300

    Orgulhoso do meu projeto!

--- aula15do9.py
+++ aula15do9.py
@@ -1,4 +1,17 @@
-n1 = int(input("Insira o número:"))
-for x in range (101,101+n1,1):
+contador = 0
+soma = 0
+contadorp = 0
+for x in range (1,11,1):
+    n1 = int(input("Insira o números:"))
+    if n1<0:
+      contador += 1
+      print(n1)
+      soma = n1 + n1
 
-    print(x, end = " " )
+contadorp= 10 -contador
+
+
+print(f"Ao total foram identificados {contadorp} positivos!")
+print(f"Ao total foram identificados {contador} negativos!")
+print(f"A soma de numeros negativos deu {soma}")
+
